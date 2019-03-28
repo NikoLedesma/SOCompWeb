d0e24175372310d87f639bfa25f4bbf2c57a4b3a
Nicolas Ledesma <nicolase.ledesma@gmail.com>
2019-03-28 00:46:36 -0300

    initial draft

---
+++ src/main/java/ar/com/SOCompWeb/SoCompWebApplication.java
@@ -0,0 +1,13 @@
+package ar.com.SOCompWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoCompWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoCompWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/SOCompWeb/SoCompWebApplicationTests.java
@@ -0,0 +1,16 @@
+package ar.com.SOCompWeb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoCompWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
